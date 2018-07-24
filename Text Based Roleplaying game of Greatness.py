1c59d48e47e86fb1e73b9fa563d8f3369129f74d
Tech3D1 <41205954+Tech3D1@users.noreply.github.com>
2018-07-24 14:58:02 -0400

    Add files via upload

--- Text Based Roleplaying game of Greatness.py
+++ Text Based Roleplaying game of Greatness.py
@@ -6,83 +6,22 @@ Created on Tue Jul 24 09:10:52 2018
 @author: stem
 """
 import random as rand
-import numpy as math
-import os
-t1 = 2
-t2 = math.sqrt(t1)
-os.system("clear")
+import time
+def divide():
+    print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
-
-#---------------------
-
-dig1 = rand.randint(0, 9)
-dig2 = rand.randint(0, 9)
-dig3 = rand.randint(0, 9)
-dig4 = rand.randint(0, 9)
-
-#---------------------
-player_wep = fists
-
-'''print("WELCOME to text adventure!")
-print("John was exploring a cave by himself and got lost deep in the cave. he decides to explore some more and gets lost inside a crypt ")
-c1 = int(input("A man wearing a ski mask jumps at you from behind and begins to drag you around. You have a choice (1): Attack (2): Surrender "))
-if c1 == 1:
-    ###
-elif c1 == 2:
-    ###
-else:
-    print("Invalid Input")'''
-
-
-p_hp = 300
-
-def combat(e_hp, e_name, p_weapon):
-    while p_hp > 0 and e_hp > 0:
-        p_turns = 0
-        e_turns = 0
-        turn = input("Would you like to Attack or Heal: ")
-        turn = turn.upper()
-        if turn == "ATTACK":
-            
-            did_hit = hit_attempt()
-            if did_hit == "hit":
-                e_hp - p_weapon
-                print("John's", p_weapon, "Hit")
-            elif did_hit == "crit":
-                e_hp - (int(p_weapon) * 2)
-                print("John's", p_weapon, "struck true, and crit", e_name)
-            elif did_hit == "miss":
-                print ("John missed the", e_name, "with the", p_weapon, "!")
-        elif turn == "HEAL":
-            if p_hp >= 300:
-                print("You are already at or above Max Health")
-            elif p_hp < 300:
-                 heal_amount = rand.randint(1, 6)
-                 p_hp + heal_amount
-                 print ("John used scraps of cloth to heal himself for", heal_amount, "!")
-    if e_hp == 0 and p_hp > 0:
-        print("The", e_name, "was killed by John!")
-    elif p_hp == 0 and e_hp > 0:
-        print("John was mortally wounded, and died!")
-    else:
-        print("Something went wrong")
-        
-        
-        
 def hit_attempt():
     hit_chance = rand.randint(1, 20)
     if hit_chance == 1:
         return "crit"
+    elif hit_chance > 15:
+        return "miss"
     else:
         return "hit"
-    if hit_chance > 15:
-        return "miss"
-
-
 # Player Weapons    
 def knife():
     #knife does 1 - 4 damage.
-    damage = rand.randint(1, 4)
+    damage = rand.randint(3, 4)
     return int(damage)
 
 def fists():
@@ -108,5 +47,96 @@ def revolver(ammo):
     else:
         print("The revolver is out of ammo...")
 
+def baseball_bat():
+    damage = 6
+    return int(damage)
 
+def combat(e_hp, e_name, p_weapon, e_dmg, p_wep_name, p_hp):
+    e_tp = 0
+    p_tp = 0
+    while p_hp > 0 and e_hp > 0:
+        while e_tp >= p_tp:
+            turn = input("Would you like to Attack or Heal: ")
+            turn = turn.upper()
+            if turn == "ATTACK":
+                p_tp = p_tp + 1
+                # print("Player points", p_tp)
+                did_hit = hit_attempt()
+                if did_hit == "hit":
+                    e_hp = e_hp - p_weapon
+                    print("John's", p_wep_name, "hit for", p_weapon, "damage!")
+                    print("The Enemy has", e_hp)
+                elif did_hit == "crit":
+                    e_hp = e_hp - (int(p_weapon) * 2)
+                    print("John's", p_wep_name, "struck true, and crit", e_name)
+                    print("The Enemy has", e_hp)
+                elif did_hit == "miss":
+                    print ("John missed the", e_name, "with the", p_wep_name, "!")
+            elif turn == "HEAL":
+                p_tp = p_tp + 1
+                if p_hp >= 300:
+                    print("You are already at or above Max Health")
+                elif p_hp < 300:
+                    heal_amount = rand.randint(1, 6)
+                    p_hp + heal_amount
+                    print ("John used scraps of cloth to heal himself for", heal_amount, "!")
+        while e_tp < p_tp:
+            e_tp = e_tp + 1
+            # print("Enemy points:", e_tp)
+            did_hit = hit_attempt()
+            if did_hit == "hit":
+                 p_hp = (p_hp - e_dmg)
+                 print("The enemy hit John")
+                 print("John has", p_hp, "Health")
+            elif did_hit == "miss":
+                 print ("The enemy missed John")
+                 print("John has", p_hp, "Health")
+                    
+    if e_hp <= 0 and p_hp > 0:
+        print("The", e_name, "was killed by John!")
+        return ("John Wins")
+    elif p_hp <= 0 and e_hp > 0:
+        print("John was mortally wounded, and died!")
+        exit()
+    else:
+        print("Something went wrong")
+# ---------------------
+# ---------------------
+dig1 = rand.randint(0, 9)
+dig2 = rand.randint(0, 9)
+dig3 = rand.randint(0, 9)
+dig4 = rand.randint(0, 9)
+# ---------------------
+play_hp = 300
+player_wep = knife()
+enemy_wep = knife()
+#combat(HP, "ENEMY NAME", player_wep, enemy_wep, "Player wep name", player's hp)
+# ---------------------
+print("John was a average man he loved taking hikes, exploring nature, and his desk job.")
+#time.sleep(3)
+print("One day John was out on a mountain trail enjoying his weekend off, when he encountered a cave.")
+#time.sleep(3)
+print("John ventured inside, not seeing the signs of recent human activity...")
+#time.sleep(3)
+c1 = input("Leaping from the shadows, a stranger in a ski mask, and heavy jacket screams and jabs at John! What does he do? (Attack, or Surrender): ")
+c1 = c1.upper()
+divide()
+if c1 == "ATTACK":
+    combat(20, "Masked Man", player_wep, enemy_wep, "knife", play_hp)
 
+else:
+    print("The masked man throws a burlap sack over John's head, and hits him...")
+    #time.sleep(2)
+    print("John awakens in darkness, with the sounds of a car around him...")
+    #time.sleep(2)
+    print("John quickly realizes that he is in a car's trunk.")
+    #time.sleep(2)
+    print("John hears voices coming around the car, and sees that he has a chance to escape by kicking the trunk open...")
+    #time.sleep(2)
+    c2 = input("What does John do?: (Wait, or Escape):")
+    c2 = c2.upper()
+    if c2 == "Escape":
+        print("John Kicks the trunk open with a loud THUD, as one of the gang members in knocked out. However one still stands...")
+        divide()
+        combat(35, "Bouncer", player_wep, enemy_wep, "knife", play_hp)
+        combat(35, "Bouncer", player_wep, enemy_wep, "knife", play_hp)
